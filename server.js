8553fafffb8ae8110168f3bb62835eb795a59be3
brooks richard <brooksrichard029@gmail.com>
2023-09-02 07:01:21 -0500

    initial setup

---
+++ server.js
@@ -0,0 +1,22 @@
+// if (process.env.NODE_env !== 'production') {
+//     require('dotenv').parse()
+// }
+
+const express = require('express')
+const app = express()
+const expressLayouts = require('express-ejs-layouts')
+
+const indexRouter = require('./routes/index')
+
+app.set('view engine', 'ejs')
+app.set('views', __dirname + '/views')
+app.set('layout', 'layouts/layout')
+app.use(expressLayouts)
+app.use(express.static('public'))
+
+// db.on('error', error => console.error(error)) 
+// db.once('open', () => console.log('Connected to database'))
+
+app.use('/', indexRouter)
+
+app.listen(process.env.PORT || 3000)
